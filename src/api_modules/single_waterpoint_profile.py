13955a87446caf1ec88c0b24f225a637380c5008
CarlosNasayo <carlosnasayov@gmail.com>
2023-09-15 16:53:58 -0500

    waterpoint profile endpoint modified

--- src/api_modules/single_waterpoint_profile.py
+++ src/api_modules/single_waterpoint_profile.py
@@ -7,6 +7,8 @@ from ormWP import Adm3
 from ormWP import Watershed
 from ormWP import Wpcontent
 from ormWP import Wscontent
+from  ormWP import Typecontent
+from mongoengine import Q
 
 
 import json
@@ -16,14 +18,14 @@ class SingleWaterpointsProfile(Resource):
     def __init__(self):
         super().__init__()
 
-    def get(self, waterpoint=None,language=None):
+    def get(self, waterpoints=None,language=None):
         """
         Get one waterpoint profile from database 
         ---
         description: Query the information of the one waterpoints profile. This endpoint has not parameter.  This endpoint needs two parameters, **waterpoint** that is id of the waterpoint to be queried (this id can be obtained from the endpoint `/waterpoints`), and the **language** that is the language that you want to get; The API will respond with the waterpoit profile with the id provided.
         parameters:
           - in: path
-            name: waterpoint
+            name: waterpoints
             type: string
             required: true
           - in: path
@@ -44,7 +46,7 @@ class SingleWaterpointsProfile(Resource):
                   description: Waterpoint name
                 ext_id:
                   type: string
-                  description: Extern Id to identify Waterpoin
+                  description: Extern Id to identify Waterpoint
                 lat:
                   type: number
                   description: latitude of the Waterpoint
@@ -80,57 +82,77 @@ class SingleWaterpointsProfile(Resource):
                   items:
                     type: string
         """
-        q_set = None
-        if waterpoint is None:
-            q_set = Waterpoint.objects()
-        else:
-            print(waterpoint)
-            q_set = Waterpoint.objects(id = waterpoint)
+        #print(waterpoints)
+        waterpointlists =list(dict.fromkeys(waterpoints.replace(" ", "").split(',')))
+        
         json_data = []
 
-        for waterpoint in q_set:
-            watershed_id = str(waterpoint.watershed.id)
-            watershed = Watershed.objects(id=watershed_id).first()
-            adminlevel3id = watershed.adm3.id
-            adm3 = Adm3.objects.get(id=adminlevel3id)
-            watershed_name = watershed.name
-            adminlevel2id = adm3.adm2.id
-            adm2 = Adm2.objects.get(id=adminlevel2id)
-            adminlevel1id = adm2.adm1.id
-            adm1 = Adm1.objects.get(id=adminlevel1id)
+        for waterpoint_id in waterpointlists:
+            q_set = Waterpoint.objects(id=waterpoint_id)
+            for waterpoint in q_set:
+                watershed_id = str(waterpoint.watershed.id)
+                watershed = Watershed.objects(id=watershed_id).first()
+                adminlevel3id = watershed.adm3.id
+                adm3 = Adm3.objects.get(id=adminlevel3id)
+                watershed_name = watershed.name
+                adminlevel2id = adm3.adm2.id
+                adm2 = Adm2.objects.get(id=adminlevel2id)
+                adminlevel1id = adm2.adm1.id
+                adm1 = Adm1.objects.get(id=adminlevel1id)
 
-            wp_contents = Wpcontent.objects(waterpoint=str(waterpoint.id))
-            ws_contents = Wscontent.objects(watershed=str(waterpoint.watershed.id))
-            contents_list = [content.content for content in wp_contents if 'language' in content.content and content.content['language'] == language]
-            for content in contents_list:
-                  if 'trace' in content:
-                      del content['trace']
+                wp_contents = Wpcontent.objects(waterpoint=str(waterpoint.id), 
+                                         content__trace__enable=True)
+                ws_contents = Wscontent.objects(watershed=str(waterpoint.watershed.id), 
+                                         content__trace__enable=True)
+                #ws_contents = Wscontent.objects(watershed=str(waterpoint.watershed.id))
+                #contents_list = [content.content for content in wp_contents if 'language' in content.content and content.content['language'] == language]
+                ids=[]
+                typecontent_name=[]
+                for i in wp_contents:
+                    ids.append(str(i.type.id))
+                    typecontent=Typecontent.objects.get(id=str(i.type.id))
+                    typecontent_name.append(typecontent.name)
 
-            contents_list_ws = [content.content for content in ws_contents if 'language' in content.content and content.content['language'] == language]
-            for content in contents_list_ws:
-                if 'trace' in content:
-                    del content['trace']
+                print(typecontent_name)
 
-            waterpoint_data = {
-                "id": str(waterpoint.id),
-                "name": waterpoint.name,
-                "lat": waterpoint.lat,
-                "lon": waterpoint.lon,
-                "area": waterpoint.area,
-                "aclimate_id":waterpoint.aclimate_id,
-                "ext_id": str(waterpoint.ext_id),
-                "watershed": watershed_id,
-                "watershed_name": watershed_name,
-                "adm3": adm3.name,
-                "adm2": adm2.name,
-                "adm1": adm1.name,
-                "contents_wp": contents_list,
-                "contents_ws": contents_list_ws,
+                print(ids)
+                #contents_list = [(content.content) for content in wp_contents if 'language' in content.content and content.content['language'] == language]
+                contents_list = [(content["content"] if "language" not in content["content"]
+                  else {**content["content"], "typecontent_id": ids.pop(0)})
+                 for content in wp_contents]
+                
+                for content, name in zip(contents_list, typecontent_name):
+    # Agregamos el nuevo ID con la clave "id" al contenido existente
+                    content["typecontent_name"] = name
 
-            }
 
-            json_data.append(waterpoint_data)
+                for content in contents_list:
+                    if 'trace' in content:
+                        del content['trace']
 
+                contents_list_ws = [content.content for content in ws_contents if 'language' in content.content and content.content['language'] == language]
+                for content in contents_list_ws:
+                    if 'trace' in content:
+                        del content['trace']
+                print(contents_list)
+                waterpoint_data = {
+                    "id": str(waterpoint.id),
+                    "name": waterpoint.name,
+                    "lat": waterpoint.lat,
+                    "lon": waterpoint.lon,
+                    "area": waterpoint.area,
+                    "aclimate_id":waterpoint.aclimate_id,
+                    "ext_id": str(waterpoint.ext_id),
+                    "watershed": watershed_id,
+                    "watershed_name": watershed_name,
+                    "adm3": adm3.name,
+                    "adm2": adm2.name,
+                    "adm1": adm1.name,
+                    "contents_wp": contents_list,
+                    "contents_ws": contents_list_ws,
+                }
+
+                json_data.append(waterpoint_data)
         return json_data
 
 
--- src/wpapi.py
+++ src/wpapi.py
@@ -13,11 +13,9 @@ from mongoengine import *
 from api_modules.adm1 import AdministrativeLevel1
 from api_modules.adm2 import AdministrativeLevel2
 from api_modules.adm3 import AdministrativeLevel3
-from api_modules.watershed import Watersheds
 from api_modules.waterpoint import Waterpoints
 from api_modules.sigle_waterpoint import SingleWaterpoints
 from api_modules.monitored_data import MonitoredData
-from api_modules.warterpoint_profile import WaterpointsProfile
 from api_modules.single_waterpoint_profile import SingleWaterpointsProfile
 from api_modules.monitored_latest  import LastMonitoredData
 #from api_modules.layers import Layers
@@ -38,13 +36,11 @@ def home():
 api.add_resource(AdministrativeLevel1, '/api/v1/adm1')
 api.add_resource(AdministrativeLevel2, '/api/v1/adm2/<adm1>')
 api.add_resource(AdministrativeLevel3, '/api/v1/adm3/<adm2>')
-api.add_resource(Watersheds, '/api/v1/watershed/<adm3>')
 api.add_resource(Waterpoints, '/api/v1/waterpoints')
 api.add_resource(SingleWaterpoints, '/api/v1/waterpoints/<waterpoint>')
 api.add_resource(MonitoredData, '/api/v1/monitored/<waterpoint>')
 api.add_resource(LastMonitoredData, '/api/v1/lastmonitored/<waterpoint>')
-api.add_resource(WaterpointsProfile, '/api/v1/waterpointsprofiles/<language>')
-api.add_resource(SingleWaterpointsProfile, '/api/v1/waterpointsprofiles/<waterpoint>/<language>')
+api.add_resource(SingleWaterpointsProfile, '/api/v1/waterpointsprofiles/<waterpoints>/<language>')
 
 
 
