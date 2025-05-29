4716dc518a8d8ac6bb6c610528547fc4bdc1aeec
CarlosNasayo <carlosnasayov@gmail.com>
2025-05-28 20:31:46 -0500

    fix getlast date

--- src/api_modules/last_monitored_by_date.py
+++ src/api_modules/last_monitored_by_date.py
@@ -2,15 +2,12 @@ from flask import Flask, jsonify
 from flask_restful import Resource
 from ormWP import Monitored, Wpcontent, Wscontent, Waterpoint, Watershed
 import json
-
 import datetime
-from flask import jsonify
 
 def find_date_in_climatology(climatology, target_date):
     target_datetime = datetime.datetime.strptime(target_date.split("T")[0] if "T" in target_date else target_date, "%Y-%m-%d")
     target_month = target_datetime.month
     target_day = target_datetime.day
-
     for daily_data in climatology:
         if daily_data[0]['month'] == target_month and daily_data[0]['day'] == target_day:
             return daily_data
@@ -21,83 +18,51 @@ class LastMonitoredDataByDate(Resource):
         super().__init__()
 
     def get(self, date=None):
-        """
-        Get last Monitored data by date
-        ---
-        description: Query the information of last monitored data based on a specific date. This endpoint needs one parameter, **date**, that is the date in format YYYY-MM-DD to be queried. The API will respond with the list of monitored values for that specific date.
-        tags:
-          - Monitored Data by Date
-        parameters:
-          - in: path
-            name: date
-            type: string
-            required: true
-            description: Date to query in format YYYY-MM-DD, for example 2024-01-29
-        responses:
-          200:
-            description: Monitored data
-            schema:
-              id: Monitored
-              properties:
-                id:
-                  type: string
-                  description: Id Monitored data
-                date:
-                  type: string
-                  description: Date of the monitored data
-                values:
-                  type: array
-                  items: {}
-                  description: List of values of the monitored data
-                waterpoint:
-                  type: string
-                  description: Id waterpoint
-        """
-
         if date is None:
             return jsonify({"error": "Date parameter is required"}), 400
 
         try:
-            target_datetime = datetime.datetime.strptime(date.split("T")[0] if "T" in date else date, "%Y-%m-%d")
-        except ValueError as e:
+            date_obj = datetime.datetime.strptime(date.split("T")[0] if "T" in date else date, "%Y-%m-%d")
+        except ValueError:
             return jsonify({"error": "Invalid date format. Use YYYY-MM-DD"}), 400
 
-        q_set = Monitored.objects(date__startswith=date)
-
         json_data = []
+        all_waterpoints = Waterpoint.objects()
 
-        for x in q_set:
-            waterpoint = x.waterpoint
-            water = Waterpoint.objects(id=waterpoint.id).first()
-            q_setWpC = Wpcontent.objects(waterpoint=waterpoint.id)
+        for wp in all_waterpoints:
+            # 1. Intentar buscar Monitored para el waterpoint en la fecha original
+            monitored = Monitored.objects(waterpoint=wp.id, date__startswith=date).first()
+            # 2. Si no hay datos, intentar en los 3 días anteriores
+            if not monitored:
+                for i in range(1, 4):
+                    previous_date = (date_obj - datetime.timedelta(days=i)).strftime("%Y-%m-%d")
+                    monitored = Monitored.objects(waterpoint=wp.id, date__startswith=previous_date).first()
+                    if monitored:
+                        break  # Encontró datos para este punto
+
+            if not monitored:
+                continue  # Este waterpoint no tiene datos en la fecha original ni en los 3 días anteriores
+
+            water = wp
+            q_setWpC = Wpcontent.objects(waterpoint=wp.id)
             q_setWsC = Wscontent.objects(watershed=water.watershed.id)
-
-            En = False
-            Am = False
-            Or = False
-
+            En = Am = Or = False
             for wsc in q_setWsC:
-                if wsc.content['language'] == 'en':
-                    En = True
-                elif wsc.content['language'] == 'or':
-                    Or = True
-                elif wsc.content['language'] == 'am':
-                    Am = True
-
+                lang = wsc.content.get('language')
+                if lang == 'en': En = True
+                elif lang == 'or': Or = True
+                elif lang == 'am': Am = True
             for wpc in q_setWpC:
-                if wpc.content['language'] == 'en':
-                    En = True
-                elif wpc.content['language'] == 'or':
-                    Or = True
-                elif wpc.content['language'] == 'am':
-                    Am = True
+                lang = wpc.content.get('language')
+                if lang == 'en': En = True
+                elif lang == 'or': Or = True
+                elif lang == 'am': Am = True
 
-            climatology_data = find_date_in_climatology(water.climatology, x.date.isoformat())
-            x.values.append(climatology_data)
+            climatology_data = find_date_in_climatology(water.climatology, monitored.date.isoformat())
+            monitored.values.append(climatology_data)
 
             if climatology_data:
-                previous_values = [{"type": item["type"], "value": item["value"]} for item in x.values if "type" in item and "value" in item]
-
+                previous_values = [{"type": item["type"], "value": item["value"]} for item in monitored.values if "type" in item and "value" in item]
                 climatology_values = []
                 for climatology_item in climatology_data:
                     for value in climatology_item.get("values", []):
@@ -105,14 +70,14 @@ class LastMonitoredDataByDate(Resource):
                             "type": "climatology_" + value["type"],
                             "value": value["value"]
                         })
-
                 all_values = previous_values + climatology_values
 
                 json_data.append({
-                    "id": str(x.id),
-                    "date": x.date.isoformat(),
+                    "id": str(monitored.id),
+                    "date": monitored.date.isoformat(),
+                    "source_date": monitored.date.isoformat(),
                     "values": all_values,
-                    "waterpointId": str(waterpoint.id),
+                    "waterpointId": str(wp.id),
                     "am": Am,
                     "en": En,
                     "or": Or
